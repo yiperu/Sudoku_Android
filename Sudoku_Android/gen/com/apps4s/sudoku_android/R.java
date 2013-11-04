52f823040db6748c481f4b6c240b615e67b3e750
Henry AT <yiperu@gmail.com>
2013-11-04 18:31:18 -0300

    se implemento el activity about con estilo alerta

--- Sudoku_Android/gen/com/apps4s/sudoku_android/R.java
+++ Sudoku_Android/gen/com/apps4s/sudoku_android/R.java
@@ -34,13 +34,17 @@ public final class R {
         public static final int new_button=0x7f090001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_about=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int about=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
         public static final int about_label=0x7f060006;
+        public static final int about_text=0x7f06000a;
+        public static final int about_title=0x7f060009;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int continue_label=0x7f060004;
@@ -48,6 +52,7 @@ public final class R {
         public static final int hello_world=0x7f060002;
         public static final int main_title=0x7f060003;
         public static final int new_game_label=0x7f060005;
+        public static final int title_activity_about=0x7f060008;
     }
     public static final class style {
         /** 
--- Sudoku_Android/src/com/apps4s/sudoku_android/MainActivity.java
+++ Sudoku_Android/src/com/apps4s/sudoku_android/MainActivity.java
@@ -2,14 +2,29 @@ package com.apps4s.sudoku_android;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity  implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Configurar escuchadores del Click para todos los botones
+        Button aboutBoton = (Button)findViewById(R.id.about_button);
+        aboutBoton.setOnClickListener(this);
+        Button continueBoton = (Button)findViewById(R.id.continue_button);
+        continueBoton.setOnClickListener(this);
+        Button nuevoBoton = (Button)findViewById(R.id.new_button);
+        nuevoBoton.setOnClickListener(this);
+        Button exitBoton = (Button)findViewById(R.id.exit_button);
+        exitBoton.setOnClickListener(this);
+        
     }
 
 
@@ -19,5 +34,20 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.about_button:
+			Intent i = new Intent(this,AboutActivity.class);
+			startActivity(i);
+			break;
+
+		default:
+			break;
+		}
+	}
     
 }
